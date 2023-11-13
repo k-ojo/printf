5bc32b2ac571ca0eaad2f669c2d1f26b914e8288
Kojo <gideonad4@gmail.com>
2023-11-13 04:39:01 -0800

    added _printf file

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.c"
+
+/**
+ * _printf- custom printf function
+ *
+ * @format: Input string
+ *
+ * @Return: number of printed characters
+ */
+int _printf(char *format, ...)
+{
+	return (0);
+}
