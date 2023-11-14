5bf98f2250927e059a87e6ec0e9d54fa6317f0ca
abidatu20196 <tahiduabida@gmail.com>
2023-11-14 06:29:18 -0800

    functions

---
+++ 0-print_%.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_37 - prints conversion % sign
+ *
+ * Return: the number characters printed
+ */
+
+int print_37(void)
+{
+	_putchar(37);
+	return (1);
+}
--- main.h
+++ main.h
@@ -9,4 +9,6 @@ int print_int(int n);
 int _pow(int base, int index);
 int print_string(char *str);
 void put_special(char c);
+int print_37(void);
+
 #endif
