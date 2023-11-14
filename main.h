4cabf83702780fca0d14d4fb29b4194445db721c
abidatu20196 <tahiduabida@gmail.com>
2023-11-14 03:06:35 -0800

    Education is when you read the fine print

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
 #include <unistd.h>
 
 int _putchar(char c);
+int _printf(const char *format, ...);
 int print_int(int n);
 int _pow(int base, int index);
 int print_string(char *str);
