e23ef094c2f64170a9aacf812f5e967356bf662d
abidatu20196 <tahiduabida@gmail.com>
2023-11-15 05:01:23 -0800

    Print is the sharpest and the strongest weapon of our party

--- 13-print_rev.c
+++ 13-print_rev.c
@@ -3,22 +3,22 @@
 
 /**
  * print_rev - prints a string in reverse
- * @args: A va_list containing the string to be printed
- *
+ * @args: va_list containing the string to be printed
  * Return: The length of the string
  */
 int print_rev(va_list args)
 {
-	char *input_string = va_arg(args, char *);
-	int str_length, index = 0;
 
-	if (input_string == NULL)
-		input_string = "(null)";
-	str_length = 0;
-	while (input_string[str_length] != '\0')
-		str_length++;
-	for (index = str_length - 1; index >= 0; index--)
-		_putchar(input_string[index]);
-	return (str_length);
+	char *s = va_arg(args, char*);
+	int i;
+	int j = 0;
+
+	if (s == NULL)
+		s = "(null)";
+	while (s[j] != '\0')
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		_putchar(s[i]);
+	return (j);
 }
 
